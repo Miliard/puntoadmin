8a0a41808ad5965d6681ed915685be114afbd015
wil <elsalvadorazul@outlook.es>
2023-05-24 16:47:38 -0600

    minuto9.21

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::redirect('/', '/login');
+// Route::redirect('/', '/login');
 
 
 Route::middleware([
