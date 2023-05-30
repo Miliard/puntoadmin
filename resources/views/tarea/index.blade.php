94bbc231312caf6eb6ab0f0e075f94a4bdcb4658
telemaco <elchwplm@gmail.com>
2023-05-29 21:38:45 -0600

    1 hora 9 minutos

--- resources/views/tarea/index.blade.php
+++ resources/views/tarea/index.blade.php
@@ -1,50 +1,51 @@
 @extends('tema.app')
 
-@section('title', "listados de Tareas")
+@section('title', 'listados de Tareas')
 
 @section('contenido')
 
-<h3>
-  listados de Tareas
-</h3>
-<table class="table table-stripped table-hover">
-    <thead>
-        <tr>
-            <th>
-                Nombre
-            </th>
-            <th>
-                Finalizada
-            </th>
-            <th>
-                Fecha limite
-            </th>
-            <th>
-                Urgencia
-            </th>
-            <th>
-                Descripcion
-            </th>
-            <th>
-                opciones
-            </th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($tareas as $tarea)
+    <h3>
+        listados de Tareas
+    </h3>
+    <table class="table table-stripped table-hover">
+        <thead>
             <tr>
-                <td>{{$tarea->nombre}}</td>
-                <td>{{$tarea->finalizado}}</td>
-                <td>{{$tarea->fecha_limite}}</td>
-                <td>{{$tarea->urgencia}}</td>
-                <td>{{$tarea->descripcion}}</td>
-                <td>
-                    
-                </td>
-
+                <th>
+                    Nombre
+                </th>
+                <th>
+                    Finalizada
+                </th>
+                <th>
+                    Fecha limite
+                </th>
+                <th>
+                    Urgencia
+                </th>
+                <th>
+                    Descripcion
+                </th>
+                <th>
+                    opciones
+                </th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($tareas as $tarea)
+                <tr>
+                    <td>{{ $tarea->nombre }}</td>
+                    <td>{{ $tarea->estaFinalizada() }}</td>
+                    <td>{{ \Carbon\Carbon::parse($tarea->fecha_limite)->format('h:i A d / m / Y') }}</td>
+
+                    <td>{{ $tarea->urgencia() }}</td>
+                    <td>{{ $tarea->descripcion }}</td>
+                    <td>
+
+                    </td>
+
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 @endsection
